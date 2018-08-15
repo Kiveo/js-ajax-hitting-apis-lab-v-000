40619980a2b09f956568ed2262ed81216096bf84
Maverick Tesini <mavericktesini@gmail.com>
2018-08-15 16:14:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-Create a form with a username field that calls a getRepositories function 
+Create a form with a username field that calls a getRepositories function
 
-that loads the repositories div with a list of public repositories for that user. 
+that loads the repositories div with a list of public repositories for that user.
 
 The displayed repositories should include the name and a link to the URL (HTML URL, not API URL).
