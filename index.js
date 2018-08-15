a36144436bf52d2e74c9989e188e92387144ce2f
Maverick Tesini <mavericktesini@gmail.com>
2018-08-15 16:37:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+// Request
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener("load", displayRepositories());
   req.open("GET", 'https://api.github.com/users/octocat/repos');
   req.send();
 }
-// called within getRepos: Handler
+// Response
+// called within getRepos: Handle + Display
 function displayRepositories(event, data) {
+  // parse responseText as JSON
   var repos = JSON.parse(this.responseText);
+  // iterate and format the output
   const repoList = `<ul>${repos.map(repo => '<li>' + repo.name + '</li>').join('')}`
+  // place the formatted output into our DOM
   document.getElementById("repositories").innerHTML = repoList;
 }
